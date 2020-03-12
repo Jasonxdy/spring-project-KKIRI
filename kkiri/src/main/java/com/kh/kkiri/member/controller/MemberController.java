777fc49943112171dc8d00e2ee654752a1655d65
유카인 <60214185+viper2605@users.noreply.github.com>
2020-03-12 16:16:02 +0900

    @service 추가

--- kkiri/src/main/java/com/kh/kkiri/member/controller/MemberController.java
+++ kkiri/src/main/java/com/kh/kkiri/member/controller/MemberController.java
@@ -2,9 +2,12 @@ package com.kh.kkiri.member.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.kh.kkiri.member.model.vo.Member;
 import com.kh.kkiri.member.service.MemberService;
 
 
@@ -16,8 +19,28 @@ import com.kh.kkiri.member.service.MemberService;
 
 public class MemberController {
 
+	
 	@Autowired
 	private MemberService memberService;
+	
+	// 6번 @세션 어트리뷰트 사용하기
+	@RequestMapping(value="login", method=RequestMethod.POST)
+	public String signIn(Member member, Model model ) {
+		
+		try {
+			
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+			
+			return null;
+			
+		}
+		
+		return null;
+	}
+	
+	
 	
 	
 	
--- kkiri/src/main/java/com/kh/kkiri/member/service/MemberServiceImpl.java
+++ kkiri/src/main/java/com/kh/kkiri/member/service/MemberServiceImpl.java
@@ -1,5 +1,12 @@
 package com.kh.kkiri.member.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
+
 public class MemberServiceImpl implements MemberService{
 
+	
+	
+	
 }
