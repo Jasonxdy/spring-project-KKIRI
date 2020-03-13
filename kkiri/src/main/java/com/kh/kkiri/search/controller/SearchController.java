7d10f6c3c978bac287e88f93bb2dc103ad64d01a
정따조 <49194647+jddajo@users.noreply.github.com>
2020-03-13 17:48:23 +0900

    젠장

--- kkiri/src/main/java/com/kh/kkiri/search/controller/SearchController.java
+++ kkiri/src/main/java/com/kh/kkiri/search/controller/SearchController.java
@@ -1,10 +1,19 @@
 package com.kh.kkiri.search.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.kh.kkiri.common.Pagination;
+import com.kh.kkiri.common.vo.PageInfo;
 import com.kh.kkiri.search.model.service.SearchService;
 import com.kh.kkiri.search.model.vo.Search;
 
@@ -16,14 +25,44 @@ public class SearchController {
 	@Autowired
 	private SearchService searchService;
 	
+	private int limit=10;
+	private int pagingBarSize=10;
+	
 	@RequestMapping("searchEvent")
-	public String searchEvent() {
+	public String searchEvent(Model model,
+								@RequestParam(value="searchKey", required=false) String searchKey,
+								@RequestParam(value="searchValue", required=false) String searchValue,
+								@RequestParam(value="currentPage", required=false) Integer currentPage
+								) {
 		try {
-			Search search = searchService.selectSearchList();
+			Map<String, String> map = null;
+			if(searchKey != null && searchValue != null) {
+				map = new HashMap<String, String>();
+				map.put("searchKey", searchKey);
+				map.put("searchValue", searchValue);
+			}
+			
+			int listCount = searchService.getSearchCount(map);
+			
+			if(currentPage == null) currentPage = 1;
+			
+			PageInfo pInf = Pagination.getPageInfo(limit, pagingBarSize, currentPage, listCount);
+			
+			List<Search> search = searchService.selectList(map, pInf);
+			
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 MM월 dd일 E요일 a h시 mm분 ss초");
+			System.out.println(search.get(0).getEventStart());
+			//SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 MM월 dd일 hh:mm");
+			
+			model.addAttribute("search", search);
+			model.addAttribute("pInf", pInf);
+			
+			return "search/searchEvent";
+			
 		} catch(Exception e) {
 			e.printStackTrace();
+			model.addAttribute("errorMsg", "탐색페이지 목록 조회중 오류발생");
+			return "common/errorPage";
 		}
-		
-		return "search/searchEvent";
 	}
 }
--- kkiri/src/main/java/com/kh/kkiri/search/model/service/SearchService.java
+++ kkiri/src/main/java/com/kh/kkiri/search/model/service/SearchService.java
@@ -1,12 +1,30 @@
 package com.kh.kkiri.search.model.service;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
 
+import com.kh.kkiri.common.vo.PageInfo;
 import com.kh.kkiri.search.model.vo.Search;
 
 @Service
 public interface SearchService {
 
-	public abstract Search selectSearchList() throws Exception;
+	/** 탐색페이지 이벤트 수 조회용 Service
+	 * @param map
+	 * @return searchCount
+	 * @throws Exception
+	 */
+	public abstract int getSearchCount(Map<String, String> map) throws Exception;
+
+	
+	/** 탐색 목록 조회용 Service
+	 * @param map
+	 * @param pInf
+	 * @return list
+	 * @throws Exception
+	 */
+	public abstract List<Search> selectList(Map<String, String> map, PageInfo pInf) throws Exception;
 
 }
