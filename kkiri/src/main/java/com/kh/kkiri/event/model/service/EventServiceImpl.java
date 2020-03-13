1ffb2b9fe8f135612a0c3d88a583737d080cbb95
Jasonxdy <Jasonxdy@users.noreply.github.com>
2020-03-13 17:55:52 +0900

    이벤트 상세 조회

--- kkiri/src/main/java/com/kh/kkiri/event/model/service/EventServiceImpl.java
+++ kkiri/src/main/java/com/kh/kkiri/event/model/service/EventServiceImpl.java
@@ -4,11 +4,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kh.kkiri.event.model.dao.EventDAO;
+import com.kh.kkiri.event.model.vo.Event;
+import com.kh.kkiri.member.model.vo.Member;
 
 @Service
 public class EventServiceImpl implements EventService {
 	
 	@Autowired
 	private EventDAO eventDAO;
+	
+	/**
+	 * 이벤트 상세 조회 Service
+	 * @param eventNo
+	 * @return event
+	 * @throws Exception
+	 */
+	@Override
+	public Event selectEvent(int eventNo) throws Exception {
+		return eventDAO.selectEvent(eventNo);
+	}
+	
+	/**
+	 * 이벤트 주최자 정보 조회
+	 * @param eventNo
+	 * @return member
+	 * @throws Exception
+	 */
+	@Override
+	public Member selectMember(int eventNo) throws Exception {
+		return eventDAO.selectMember(eventNo);
+	}
 
 }
--- kkiri/src/main/java/com/kh/kkiri/event/model/vo/Event.java
+++ kkiri/src/main/java/com/kh/kkiri/event/model/vo/Event.java
@@ -8,12 +8,141 @@ public class Event {
 	private String eventContent;
 	private String eventCategory;
 	private String eventLocation;
-	private int latitude;
-	private int longtitude;
+	private double latitude;
+	private double longtitude;
 	private Date eventStart;
 	private Date eventEnd;
 	private int eventTicket;
 	private String eventThumbnail;
 	private int eventQuota;
-	private String eventStaus;
+	
+	public Event() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Event(int eventNo, String eventTitle, String eventContent, String eventCategory, String eventLocation,
+			double latitude, double longtitude, Date eventStart, Date eventEnd, int eventTicket, String eventThumbnail,
+			int eventQuota) {
+		super();
+		this.eventNo = eventNo;
+		this.eventTitle = eventTitle;
+		this.eventContent = eventContent;
+		this.eventCategory = eventCategory;
+		this.eventLocation = eventLocation;
+		this.latitude = latitude;
+		this.longtitude = longtitude;
+		this.eventStart = eventStart;
+		this.eventEnd = eventEnd;
+		this.eventTicket = eventTicket;
+		this.eventThumbnail = eventThumbnail;
+		this.eventQuota = eventQuota;
+	}
+
+	public int getEventNo() {
+		return eventNo;
+	}
+
+	public void setEventNo(int eventNo) {
+		this.eventNo = eventNo;
+	}
+
+	public String getEventTitle() {
+		return eventTitle;
+	}
+
+	public void setEventTitle(String eventTitle) {
+		this.eventTitle = eventTitle;
+	}
+
+	public String getEventContent() {
+		return eventContent;
+	}
+
+	public void setEventContent(String eventContent) {
+		this.eventContent = eventContent;
+	}
+
+	public String getEventCategory() {
+		return eventCategory;
+	}
+
+	public void setEventCategory(String eventCategory) {
+		this.eventCategory = eventCategory;
+	}
+
+	public String getEventLocation() {
+		return eventLocation;
+	}
+
+	public void setEventLocation(String eventLocation) {
+		this.eventLocation = eventLocation;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLongtitude() {
+		return longtitude;
+	}
+
+	public void setLongtitude(double longtitude) {
+		this.longtitude = longtitude;
+	}
+
+	public Date getEventStart() {
+		return eventStart;
+	}
+
+	public void setEventStart(Date eventStart) {
+		this.eventStart = eventStart;
+	}
+
+	public Date getEventEnd() {
+		return eventEnd;
+	}
+
+	public void setEventEnd(Date eventEnd) {
+		this.eventEnd = eventEnd;
+	}
+
+	public int getEventTicket() {
+		return eventTicket;
+	}
+
+	public void setEventTicket(int eventTicket) {
+		this.eventTicket = eventTicket;
+	}
+
+	public String getEventThumbnail() {
+		return eventThumbnail;
+	}
+
+	public void setEventThumbnail(String eventThumbnail) {
+		this.eventThumbnail = eventThumbnail;
+	}
+
+	public int getEventQuota() {
+		return eventQuota;
+	}
+
+	public void setEventQuota(int eventQuota) {
+		this.eventQuota = eventQuota;
+	}
+
+	@Override
+	public String toString() {
+		return "Event [eventNo=" + eventNo + ", eventTitle=" + eventTitle + ", eventContent=" + eventContent
+				+ ", eventCategory=" + eventCategory + ", eventLocation=" + eventLocation + ", latitude=" + latitude
+				+ ", longtitude=" + longtitude + ", eventStart=" + eventStart + ", eventEnd=" + eventEnd
+				+ ", eventTicket=" + eventTicket + ", eventThumbnail=" + eventThumbnail + ", eventQuota=" + eventQuota
+				+ "]";
+	}
+	
+	
+	
 }
